940abc91e9d84db538ed79439efa382447e5c93d
Clement Sanchez <sanchez.clmnt@gmail.com>
2017-10-23 20:18:37 +0200

    make the attributes of the abstract class Vehicule

---
+++ entites/Vehicule.php
@@ -0,0 +1,20 @@
+<?php
+ /**
+ *
+ */
+abstract class Vehicule
+{
+  protected $id;
+  protected $type;
+  protected $brand;
+  protected $modele;
+  protected $immatriculation;
+  protected $price;
+  protected $description;
+  function __construct()
+  {
+    # code...
+  }
+}
+
+ ?>
