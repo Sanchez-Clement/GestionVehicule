8e1c8e0b3ff0c7084587b5792c1e98215df6e335
Clement Sanchez <sanchez.clmnt@gmail.com>
2017-10-23 20:48:56 +0200

    add all the getters for the abstract class Vehicule

--- entites/Vehicule.php
+++ entites/Vehicule.php
@@ -2,19 +2,48 @@
  /**
  *
  */
-abstract class Vehicule
+class Vehicule
 {
-  protected $id;
-  protected $type;
-  protected $brand;
-  protected $modele;
-  protected $immatriculation;
-  protected $price;
-  protected $description;
-  function __construct()
-  {
-    # code...
-  }
-}
+    protected $id;
+    protected $type ;
+    protected $brand;
+    protected $modele;
+    protected $immatriculation;
+    protected $price;
+    protected $description;
+    public function __construct()
+    {
+        $this->type=static::class;
+    }
 
- ?>
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getType()
+    {
+        return $this->type;
+    }
+    public function getBrand()
+    {
+        return $this->brand;
+    }
+    public function getModele()
+    {
+        return $this->modele;
+    }
+    public function getImmatriculation()
+    {
+        return $this->immatriculation;
+    }
+    public function getPrice()
+    {
+        return $this->price;
+    }
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+}
--- index.php
+++ index.php
@@ -1,1 +1,6 @@
+<?php
+require "entites/Vehicule.php";
 
+$vbj= new Vehicule;
+echo $vbj->getType()
+?>
