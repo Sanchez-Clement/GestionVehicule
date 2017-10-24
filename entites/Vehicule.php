7119d2338c03db28c040860b5d58b991832aee16
Clement Sanchez <sanchez.clmnt@gmail.com>
2017-10-24 10:49:55 +0200

    make all the setters for the abstract class Vehicule

--- entites/Vehicule.php
+++ entites/Vehicule.php
@@ -2,7 +2,7 @@
  /**
  *
  */
-class Vehicule
+abstract class Vehicule
 {
     protected $id;
     protected $type ;
@@ -11,9 +11,10 @@ class Vehicule
     protected $immatriculation;
     protected $price;
     protected $description;
-    public function __construct()
+    public function __construct(array $donnees)
     {
         $this->type=static::class;
+        $this->hydrate($donnees);
     }
 
     public function getId()
@@ -44,6 +45,97 @@ class Vehicule
     public function getDescription()
     {
         return $this->description;
+    }
+
+    /**
+     * Set the value of Id
+     *
+     * @param mixed id
+     *
+     * @return self
+     */
+    public function setId($id)
+    {
+        $id = (int)$id;
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Set the value of Brand
+     *
+     * @param mixed brand
+     *
+     * @return self
+     */
+    public function setBrand($brand)
+    {
+      if(is_string($brand)) {
+        $this->brand = $brand;
+
+        return $this;}
+    }
+
+    /**
+     * Set the value of Modele
+     *
+     * @param mixed modele
+     *
+     * @return self
+     */
+    public function setModele($modele)
+    {
+      if(is_string($modele)) {
+        $this->modele = $modele;
+
+        return $this;}
+    }
+
+    /**
+     * Set the value of Immatriculation
+     *
+     * @param mixed immatriculation
+     *
+     * @return self
+     */
+    public function setImmatriculation($immatriculation)
+    {
+      if(is_string($immatriculation)) {
+        $this->immatriculation = $immatriculation;
+
+        return $this;}
+    }
+
+    /**
+     * Set the value of Price
+     *
+     * @param mixed price
+     *
+     * @return self
+     */
+    public function setPrice($price)
+    {
+      $price = (int)$price;
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Set the value of Description
+     *
+     * @param mixed description
+     *
+     * @return self
+     */
+    public function setDescription($description)
+    {
+      if(is_string($description)){
+        $this->description = $description;
+
+        return $this;
+        }
     }
 
 }
