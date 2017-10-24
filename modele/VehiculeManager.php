8b023c7f9d38b7f443d90ed2c7e7106aeb085c92
Clement Sanchez <sanchez.clmnt@gmail.com>
2017-10-24 12:08:54 +0200

    make the method getVehicules for the class VehiculeManager

--- modele/VehiculeManager.php
+++ modele/VehiculeManager.php
@@ -13,6 +13,32 @@ class VehiculeManager
   {
     $this->bdd=$bdd;
   }
+  public function getVehicules(){
+    $vehicules = [];
+    $reponse = $this->bdd->query('SELECT * FROM vehicule');
+
+    while ($donnees = $reponse->fetch(PDO::FETCH_ASSOC)) {
+      switch ($donnees['type']) {
+        case 'Moto':
+          $vehicules[]= new Moto($donnees);
+          break;
+
+        case 'Voiture':
+          $vehicules[]= new Voiture($donnees);
+          break;
+
+        case 'Camion':
+          $vehicules[]= new Camion($donnes);
+          break;
+
+        default:
+
+          break;
+      }
+    }
+
+    return $vehicules;
+  }
 }
 
  ?>
