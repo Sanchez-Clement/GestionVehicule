053136216b2c0cdfa5640d91558c205a0244d89f
Clement Sanchez <sanchez.clmnt@gmail.com>
2017-10-24 10:56:53 +0200

    make the method hydrate for the abstract class Vehicule

--- entites/Vehicule.php
+++ entites/Vehicule.php
@@ -17,6 +17,16 @@ abstract class Vehicule
         $this->hydrate($donnees);
     }
 
+    public function hydrate(array $donnes)
+    {
+      foreach ($donnees as $key => $value) {
+          $method = "set" . ucfirst($key) ;
+          if (method_exists($this, $method)) {
+              $this->$method($value);
+          }
+      }
+    }
+
     public function getId()
     {
         return $this->id;
